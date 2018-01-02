b1f8739761cc172cfed81fd85c1ebe22cc144b6b
Tadek Teleżyński <tadekte@gmail.com>
2018-01-02 16:18:27 +0100

    remove comments from setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1',
     description='Django app which provides abstract SEO mixin and a corresponding template to override.',
     author='Laboratorium EE',
-    packages=['ee_seo_mixin'],  # same as name
-    install_requires=['django==1.11'],  # external packages as dependencies
+    packages=['ee_seo_mixin'],
+    install_requires=['django==1.11'],
 )
