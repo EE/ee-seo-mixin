e8b335438242533f4a0bfca84ef3d238984a6420
Tadek Teleżyński <tadekte@gmail.com>
2018-01-02 16:20:53 +0100

    add app and setup.py

---
+++ ee_seo_mixin/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class SearchEngineOptimizableEntity(models.Model):
+    class Meta:
+        abstract = True
+
+    meta_description_override = models.TextField(
+        verbose_name=_("SEO: Meta description override"),
+        null=True,
+        blank=True,
+        max_length=500)
+    title_override = models.CharField(
+        verbose_name=_("SEO: Title override"),
+        null=True,
+        blank=True,
+        max_length=120)
+    block_indexing = models.BooleanField(
+        verbose_name=_("SEO: Block indexing by search engine robots"),
+        default=False)
